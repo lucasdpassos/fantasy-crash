aa1a2b91e28514108cbe32562e446f1f0daf9ce8
Lucas Passos <lucas.depassos@yahoo.com.br>
2023-11-02 01:30:18 -0300

    added sword element and axis lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import swordImage from '../public/assets/sword.jpeg'; // Importe a imagem
+import crashImage from '../public/assets/crash.png'; // Importe a imagem do crash
 
 const App = () => {
   const [randomValue, setRandomValue] = useState(0);
@@ -50,7 +51,24 @@ const App = () => {
           <source src="./assets/space.mp4" type="video/mp4" />
           Seu navegador não suporta a tag de vídeo.
         </video>
-        <img src={swordImage} alt="Espada" className="absolute top-1/2 right-0 transform -translate-y-1/2 w-96 h-auto" />
+
+        {/* Adicionar as linhas (eixo x e eixo y) e a imagem do crash no ponto (0, 0) */}
+        <div className="absolute inset-0">
+          <div className="border-b border-white w-full absolute top-1/2 left-0"></div>
+          <div className="border-r border-white h-full absolute top-0 left-1/2"></div>
+          <img
+            src={crashImage}
+            alt="Crash"
+            className={`absolute bottom-0 left-0 w-24 h-auto ${isWaiting ? '' : 'animate-bounce'}`}
+          />
+        </div>
+
+        {/* Ajustar a posição da imagem "sword" para o lado direito usando CSS */}
+        <img
+          src={swordImage}
+          alt="Espada"
+          className="absolute top-1/2 right-0 transform -translate-y-1/2 w-96 h-auto"
+        />
       </div>
 
       {/* Exibir o valor atual do número animado ou a mensagem de espera */}
