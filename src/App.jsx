1f627a822856b3b216336a07b5c0fe5219fccba8
Lucas Passos <lucas.depassos@yahoo.com.br>
2023-11-02 01:55:50 -0300

    added last 15 results historic

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import swordImage from '../public/assets/sword.jpeg'; // Importe a imagem
-import crashImage from '../public/assets/crash.png'; // Importe a imagem do crash
+import swordImage from '../public/assets/sword.jpeg';
+import crashImage from '../public/assets/crash.png';
+import ResultList from './components/resultList';
 
 const App = () => {
   const [randomValue, setRandomValue] = useState(0);
   const [currentValue, setCurrentValue] = useState(0);
   const [isWaiting, setIsWaiting] = useState(false);
+  const [results, setResults] = useState([]);
+  const [isAnimating, setIsAnimating] = useState(false);
 
   useEffect(() => {
-    const animationDuration = 3000; // Duração da animação em milissegundos
-    const waitDuration = 2000; // Duração da pausa em milissegundos após atingir o valor gerado
-    const waitMessageDuration = 10000; // Duração da mensagem de espera em milissegundos
-
-    const step = 1; // Valor de incremento a cada iteração
+    const animationDuration = 3000;
+    const waitDuration = 2000;
+    const waitMessageDuration = 10000;
 
     const animate = () => {
-      const random = Math.floor(Math.random() * 9999) + 1;
-      setRandomValue(random);
-
-      let stepCount = 0;
+      let random = 0;
+      setIsAnimating(true);
 
       const animationInterval = setInterval(() => {
-        if (stepCount < random) {
+        if (currentValue < random) {
           setCurrentValue((prevValue) => {
             if (prevValue < random) {
-              return prevValue + step;
+              return prevValue + 1;
             }
             return prevValue;
           });
-          stepCount += 1;
         } else {
           clearInterval(animationInterval);
+          setIsAnimating(false);
           setIsWaiting(true);
           setTimeout(() => {
             setIsWaiting(false);
-            setCurrentValue(0); // Redefinir para zero após a pausa
-            animate(); // Iniciar a animação novamente
+            setResults((prevResults) => [random, ...prevResults].slice(0, 15));
+            generateRandom();
           }, waitMessageDuration);
         }
-      }, animationDuration / random);
+      }, (animationDuration / random) * 0.1);
+
+      const generateRandom = () => {
+        random = Math.floor(Math.random() * 9999) + 1;
+        setRandomValue(random);
+        setCurrentValue(0);
+        setIsAnimating(true);
+      };
+
+      generateRandom();
     };
 
-    animate(); // Iniciar a primeira animação
-  }, []); // Use um array vazio para garantir que este efeito seja executado apenas uma vez
+    animate();
+  }, []);
 
   return (
     <div className="bg-black h-screen flex flex-col justify-center items-center relative">
@@ -52,18 +60,18 @@ const App = () => {
           Seu navegador não suporta a tag de vídeo.
         </video>
 
-        {/* Adicionar as linhas (eixo x e eixo y) e a imagem do crash no ponto (0, 0) */}
         <div className="absolute inset-0">
           <div className="border-b border-white w-full absolute top-1/2 left-0"></div>
           <div className="border-r border-white h-full absolute top-0 left-1/2"></div>
           <img
             src={crashImage}
             alt="Crash"
-            className={`absolute bottom-0 left-0 w-24 h-auto ${isWaiting ? '' : 'animate-bounce'}`}
+            className={`absolute bottom-0 left-0 w-24 h-auto ${
+              isAnimating ? 'animate-bounce' : ''
+            }`}
           />
         </div>
 
-        {/* Ajustar a posição da imagem "sword" para o lado direito usando CSS */}
         <img
           src={swordImage}
           alt="Espada"
@@ -71,12 +79,11 @@ const App = () => {
         />
       </div>
 
-      {/* Exibir o valor atual do número animado ou a mensagem de espera */}
       <div className="text-white text-4xl mb-8">
         {isWaiting ? 'Waiting for the next round ;)' : currentValue}
       </div>
 
-      {/* Renderizar componentes de jogo aqui */}
+      <ResultList results={results} />
     </div>
   );
 };
